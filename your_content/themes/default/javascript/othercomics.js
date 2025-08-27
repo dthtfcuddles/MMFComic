5894de1f945aa431c54d3361d623c39f6c41ef81
htfcuddles <d.t.victr@gmail.com>
2025-08-27 18:12:50 -0400

    Update othercomics.js

--- your_content/themes/default/javascript/othercomics.js
+++ your_content/themes/default/javascript/othercomics.js
@@ -1,13 +1,5 @@
 <!--
 
     document.write("PLACEHOLDER LOL YEAH <br/>");
-    document.write("<table style='width: 60%; margin-left:auto; margin-right:auto;'>");
-    document.write("<tr style='height: 150px;'>");
-    document.write("<td style='text-align: center; padding-top: 10px; width:50%; vertical-align:top;'><a href='https://www.comic1.com/'><img src='/your_content/images/cast/rowi.png' /></a></td>");
-    document.write("<td style='text-align: left; padding-left: 10px; vertical-align:top;'>");
-    document.write("<a href='https://www.comic2.com/'><img src='/your_content/images/albumart/2011Foreigners.jpg'></a>");
-    document.write("</td>");
-    document.write("</tr>");
-    document.write("</table>");
 
 //-->
