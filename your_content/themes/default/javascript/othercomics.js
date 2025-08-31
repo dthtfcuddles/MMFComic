2335f27e2eb6b225a6594e7160f65eeb0c266adc
htfcuddles <d.t.victr@gmail.com>
2025-08-30 21:41:32 -0400

    Foxglove comics list

--- your_content/themes/default/javascript/othercomics.js
+++ your_content/themes/default/javascript/othercomics.js
@@ -8,19 +8,20 @@
     document.write("<td style='text-align: left; padding-left: 10px; vertical-align:top;'>");
     document.write("<a href='https://www.kalzeria.com/'><img src='/your_content/images/othercomics/KalzeriaNormal.jpg'></a>");
     document.write("</td>");
-
+    document.write("</tr>");
+    document.write("<tr style='height: 150px;'>");
     document.write("<td style='text-align: center; padding-top: 10px; width:50%; vertical-align:top;'><a href='https://www.mothorialcomic.com/'><img src='/your_content/images/othercomics/MothorialNormal.jpg' /></a></td>");
     document.write("<td style='text-align: left; padding-left: 10px; vertical-align:top;'>");
     document.write("<a href='https://tapas.io/series/PHTR6G'><img src='/your_content/images/othercomics/PreenyNormal.jpg'></a>");
     document.write("</td>");
-
-
+    document.write("</tr>");
+    document.write("<tr style='height: 150px;'>");
     document.write("<td style='text-align: center; padding-top: 10px; width:50%; vertical-align:top;'><a href='https://www.starwarriorscomic.com/'><img src='/your_content/images/othercomics/StarWarriorsNormal.jpg' /></a></td>");
     document.write("<td style='text-align: left; padding-left: 10px; vertical-align:top;'>");
     document.write("<a href='https://www.tamberlanecomic.com/'><img src='/your_content/images/othercomics/TamberlaneNormal.jpg'></a>");
     document.write("</td>");
-
     document.write("</tr>");
+
     document.write("</table>");
 
 //-->
