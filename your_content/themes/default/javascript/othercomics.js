b1ff7462d51c0fb6a8510d499b7c395269f1f3a2
htfcuddles <d.t.victr@gmail.com>
2025-08-30 21:36:25 -0400

    Foxglove collective links

--- your_content/themes/default/javascript/othercomics.js
+++ your_content/themes/default/javascript/othercomics.js
@@ -1,5 +1,27 @@
 <!--
 
-    document.write("PLACEHOLDER LOL YEAH <br/>");
+    document.write("<a href='https://www.foxglovecomics.com'>Foxglove Comics</a><br/>");
+
+    document.write("<table style='width: 60%; margin-left:auto; margin-right:auto;'>");
+    document.write("<tr style='height: 150px;'>");
+    document.write("<td style='text-align: center; padding-top: 10px; width:50%; vertical-align:top;'><a href='https://circusanimals.webcomic.ws/'><img src='/your_content/images/othercomics/CircusAnimalsNormal.jpg' /></a></td>");
+    document.write("<td style='text-align: left; padding-left: 10px; vertical-align:top;'>");
+    document.write("<a href='https://www.kalzeria.com/'><img src='/your_content/images/othercomics/KalzeriaNormal.jpg'></a>");
+    document.write("</td>");
+
+    document.write("<td style='text-align: center; padding-top: 10px; width:50%; vertical-align:top;'><a href='https://www.mothorialcomic.com/'><img src='/your_content/images/othercomics/MothorialNormal.jpg' /></a></td>");
+    document.write("<td style='text-align: left; padding-left: 10px; vertical-align:top;'>");
+    document.write("<a href='https://tapas.io/series/PHTR6G'><img src='/your_content/images/othercomics/PreenyNormal.jpg'></a>");
+    document.write("</td>");
+
+
+    document.write("<td style='text-align: center; padding-top: 10px; width:50%; vertical-align:top;'><a href='https://www.starwarriorscomic.com/'><img src='/your_content/images/othercomics/StarWarriorsNormal.jpg' /></a></td>");
+    document.write("<td style='text-align: left; padding-left: 10px; vertical-align:top;'>");
+    document.write("<a href='https://www.tamberlanecomic.com/'><img src='/your_content/images/othercomics/TamberlaneNormal.jpg'></a>");
+    document.write("</td>");
+
+    document.write("</tr>");
+    document.write("</table>");
 
 //-->
+
