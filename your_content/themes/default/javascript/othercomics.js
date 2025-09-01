8da82e9182aded5c77bad35ed09799b87178faf6
htfcuddles <d.t.victr@gmail.com>
2025-09-01 19:00:08 -0400

    target=blank on comic links

--- your_content/themes/default/javascript/othercomics.js
+++ your_content/themes/default/javascript/othercomics.js
@@ -1,26 +1,26 @@
 <!--
     document.write("<table style='width: 80%; margin-left:auto; margin-right:auto;'>");
     document.write("<tr>");
-    document.write("<td style='text-align: center; padding-top: 5px; width:100%; vertical-align:top;'><a href='https://www.foxglovecomics.com'><img src='/your_content/images/othercomics/FoxgloveFullLogo.jpg' /></td>");
+    document.write("<td style='text-align: center; padding-top: 5px; width:100%; vertical-align:top;'><a href='https://www.foxglovecomics.com' target='_blank'><img src='/your_content/images/othercomics/FoxgloveFullLogo.jpg' /></td>");
     document.write("</tr>");
     document.write("</table>");
     document.write("<table style='width: 80%; margin-left:auto; margin-right:auto;'>");
     document.write("<tr style='height: 150px;'>");
-    document.write("<td style='text-align: center; width:50%; vertical-align:top;'><a href='https://circusanimals.webcomic.ws/'><img src='/your_content/images/othercomics/CircusAnimalsNormal.jpg' /></a></td>");
+    document.write("<td style='text-align: center; width:50%; vertical-align:top;'><a href='https://circusanimals.webcomic.ws/' target='_blank'><img src='/your_content/images/othercomics/CircusAnimalsNormal.jpg' /></a></td>");
     document.write("<td style='text-align: left;  padding-left: 5px; vertical-align:top;'>");
-    document.write("<a href='https://www.kalzeria.com/'><img src='/your_content/images/othercomics/KalzeriaNormal.jpg'></a>");
+    document.write("<a href='https://www.kalzeria.com/' target='_blank'><img src='/your_content/images/othercomics/KalzeriaNormal.jpg'></a>");
     document.write("</td>");
     document.write("</tr>");
     document.write("<tr style='height: 150px;'>");
-    document.write("<td style='text-align: center; padding-top: 5px; width:50%; vertical-align:top;'><a href='https://www.mothorialcomic.com/'><img src='/your_content/images/othercomics/MothorialNormal.jpg' /></a></td>");
+    document.write("<td style='text-align: center; padding-top: 5px; width:50%; vertical-align:top;'><a href='https://www.mothorialcomic.com/' target='_blank'><img src='/your_content/images/othercomics/MothorialNormal.jpg' /></a></td>");
     document.write("<td style='text-align: left;  padding-top: 5px; padding-left: 5px; vertical-align:top;'>");
-    document.write("<a href='https://www.preenycomic.com'><img src='/your_content/images/othercomics/PreenyNormal.jpg'></a>");
+    document.write("<a href='https://www.preenycomic.com' target='_blank'><img src='/your_content/images/othercomics/PreenyNormal.jpg'></a>");
     document.write("</td>");
     document.write("</tr>");
     document.write("<tr style='height: 150px;'>");
-    document.write("<td style='text-align: center; padding-top: 5px; width:50%; vertical-align:top;'><a href='https://www.starwarriorscomic.com/'><img src='/your_content/images/othercomics/StarWarriorsNormal.jpg' /></a></td>");
+    document.write("<td style='text-align: center; padding-top: 5px; width:50%; vertical-align:top;'><a href='https://www.starwarriorscomic.com/' target='_blank'><img src='/your_content/images/othercomics/StarWarriorsNormal.jpg' /></a></td>");
     document.write("<td style='text-align: left; padding-top: 5px; padding-left: 5px; vertical-align:top;'>");
-    document.write("<a href='https://www.tamberlanecomic.com/'><img src='/your_content/images/othercomics/TamberlaneNormal.jpg'></a>");
+    document.write("<a href='https://www.tamberlanecomic.com/' target='_blank'><img src='/your_content/images/othercomics/TamberlaneNormal.jpg'></a>");
     document.write("</td>");
     document.write("</tr>");
 
