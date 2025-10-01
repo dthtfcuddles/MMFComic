aa7fa96b1016ae7c42e86956cc7a164106bc2f7f
htfcuddles <d.t.victr@gmail.com>
2025-10-01 14:50:46 -0300

    Update separator.js

--- your_content/themes/voyages/javascript/separator.js
+++ your_content/themes/voyages/javascript/separator.js
@@ -1,5 +1,3 @@
 <!--
-    randomImage = new Array(1);
-    separatorCode[0] = "<img src='/your_content/images/separator.png'>";
-    document.write(separatorCode[0]);
+    document.write("<img src='/your_content/images/separator.png'> Separator asdf");
     //-->
