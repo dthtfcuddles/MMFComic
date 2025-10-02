831be8a921417925d5d46cb26c86f0c515dad2d6
htfcuddles <d.t.victr@gmail.com>
2025-10-02 18:02:53 -0300

    Discord link in index

---
+++ your_content/themes/default/javascript/discordlink.js
@@ -0,0 +1,9 @@
+<!--
+    document.write("<table style='width: 50%; margin-left:auto; margin-right:auto;'>");
+    document.write("<tr>");
+    document.write("<td style='text-align: center; padding-top: 5px; width:100%; vertical-align:top;'><a href='https://discord.gg/FoxgloveComics' target='_blank'><img src='/your_content/images/discordimage.png' /></td>");
+    document.write("</tr>");
+    document.write("</table>");
+
+//-->
+
