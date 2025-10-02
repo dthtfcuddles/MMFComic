50da471845da3770479e7c8f51006cf8881d8125
htfcuddles <d.t.victr@gmail.com>
2025-10-02 18:07:18 -0300

    Making Discord Link use less space

--- your_content/themes/default/javascript/discordlink.js
+++ your_content/themes/default/javascript/discordlink.js
@@ -1,5 +1,5 @@
 <!--
-    document.write("<table style='width: 50%; margin-left:auto; margin-right:auto;'>");
+    document.write("<table style='width: 30%; margin-left:auto; margin-right:auto;'>");
     document.write("<tr>");
     document.write("<td style='text-align: center; padding-top: 5px; width:100%; vertical-align:top;'><a href='https://discord.gg/FoxgloveComics' target='_blank'><img src='/your_content/images/discordimage.png' /></td>");
     document.write("</tr>");
